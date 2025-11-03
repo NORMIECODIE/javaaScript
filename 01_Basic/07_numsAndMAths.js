46e44e76aacc3f36a5a5e7dbce3c2fff86387697
NORMIECODIE <deepakkrsharma76@gmail.com>
2025-11-03 10:41:20 +0530

    number and maths topic is done

---
+++ 01_Basic/07_numsAndMAths.js
@@ -0,0 +1,84 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2)); //toFixed is use to how many decimal you need 
+
+
+const anotherNumber = 233.567
+//console.log(anotherNumber.toPrecision(4)); // toPrecisionvis use to make a precise value of decimals
+
+
+const hundreds = 1000000000
+//console.log(hundreds.toLocaleString("en-In"));
+
+
+
+/*[[Prototype]]
+: 
+Number
+constructor
+: 
+ƒ Number()
+toExponential
+: 
+ƒ toExponential()
+toFixed
+: 
+ƒ toFixed()
+toLocaleString
+: 
+ƒ toLocaleString()
+toPrecision
+: 
+ƒ toPrecision()
+toString
+: 
+ƒ toString()
+valueOf
+: 
+ƒ valueOf()
+*/
+  
+
+//****************Maths*****************************
+
+
+console.log(Math);
+console.log(Math.abs(-4));// Math.abs  makes the value positive 
+console.log(Math.round(4.4));// Math.abs  makes the value round off 
+console.log(Math.ceil(4.4));// Math.abs  makes the value by priortise the upper value value becomes 5
+console.log(Math.floor(4.4));// Math.abs  makes the value by priortise the lower value value becomes 4
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+
+/*
+[[Prototype]]
+: 
+Number
+constructor
+: 
+ƒ Number()
+toExponential
+: 
+ƒ toExponential()
+toFixed
+: 
+ƒ toFixed()
+toLocaleString
+: 
+ƒ toLocaleString()
+toPrecision
+: 
+ƒ toPrecision()
+toString
+: 
+ƒ toString()
+valueOf
+: 
+ƒ valueOf()
+*/
